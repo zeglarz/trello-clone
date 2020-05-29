73eca7c2d8cf47d863105c2d60e5de4fba6dcc77
Konrad Rudnicki <zeglarz@gmail.com>
2020-05-29 14:10:42 +0200

    add window component

--- client/src/components/Window.jsx
+++ client/src/components/Window.jsx
@@ -4,7 +4,23 @@ import Modal from "react-modal";
 Modal.setAppElement("#app");
 
 export default function Window({ show, onClose, item }) {
-  return <div>
-      
-  </div>;
+  return (
+    <Modal
+      isOpen={show}
+      onRequestClose={onClose}
+      className={"modal"}
+      overlayClassName={"overlay"}
+    >
+      <div className="close-btn-ctn">
+        <h1 style={{ flex: "1 90%" }}>{item.title}</h1>
+        <button className="close-btn" onClick={onClose}>
+          X
+        </button>
+        <div>
+          <h2>Description</h2>
+          <p>{item.count}</p>
+        </div>
+      </div>
+    </Modal>
+  );
 }
